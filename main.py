59d2043ef99223f3e6cc91a69a4af4784a3b8a6c
Urip Subekti <urip.subekti13@gmail.com>
2021-06-03 13:54:36 +0700

    Fix query string

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import requests
-import urllib
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
@@ -24,7 +23,7 @@ class KeywordQueryEventListener(EventListener):
 
         if event.get_keyword() == "en2id":
             text = event.get_argument()
-            query = urllib.urlencode({"text": text})
+            query = "text=" + text.replace(" ", "+")
             url = "https://en2id-server.vercel.app/en-id?" + query
             response = requests.request("GET", url)
             data = response.json()
@@ -37,20 +36,16 @@ class KeywordQueryEventListener(EventListener):
 
         if event.get_keyword() == "id2en":
             text = event.get_argument()
-            query = text.replace(" ", "+")
-            # url = "https://en2id-server.vercel.app/id-en?" + query
-            # response = requests.request("GET", url)
-            # data = response.json()
-
-            items.append(ExtensionResultItem(icon='images/icon.png',
-                                                 name='%s' % query,
-                                                 description=''
-                                                 ))
-            # if data["text"] is not None:
-            #     items.append(ExtensionResultItem(icon='images/icon.png',
-            #                                      name='%s' % data["text"],
-            #                                      description='',
-            #                                      on_enter=OpenUrlAction('https://translate.google.co.id/?hl=id&sl=id&tl=en&%s&op=translate' % query)))
+            query = "text=" + text.replace(" ", "+")
+            url = "https://en2id-server.vercel.app/id-en?" + query
+            response = requests.request("GET", url)
+            data = response.json()
+           
+            if data["text"] is not None:
+                items.append(ExtensionResultItem(icon='images/icon.png',
+                                                 name='%s' % data["text"],
+                                                 description='',
+                                                 on_enter=OpenUrlAction('https://translate.google.co.id/?hl=id&sl=id&tl=en&%s&op=translate' % query)))
 
         return RenderResultListAction(items)
 
